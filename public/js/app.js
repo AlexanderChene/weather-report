06c363363af2e441d1286ff605f5abf69e2fd8b3
Yixiao <yixiaozhouchen@hotmail.com>
2020-05-30 11:18:07 -0400

    set up for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
 function fetchWeather(location){
-fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forcast = require('./utils/forcast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const partialsPath = path.join(__dirname, '../templates/partials')
 app.use(express.static(path.join(__dirname,'../public')))
@@ -112,6 +113,6 @@ app.get('*', (req, res)=>{
 //app.com/help
 //app.com/about
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log('Server is up on port')
 })
